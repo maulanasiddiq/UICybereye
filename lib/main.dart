6a8a0ee52fa237fcc587da688861113cdf0e0c07
Bayu Adi Prasetiyo <bayu28.bap@gmail.com>
2019-06-23 16:54:26 +0700

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:cybereye/bottom.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'theme.dart';
 
 void main(){
   runApp(MaterialApp(
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,15 +1,186 @@
 import 'package:flutter/material.dart';
+import './theme.dart';
 
 class Profile extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
 }
 
+bool isSwitched = false;
+
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Container(
+                color: Colors.blue,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                  child: Column(
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Text(
+                          "Profile",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        ),
+                      ]),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: ExactAssetImage('assets/logo_cc.png'),
+                        minRadius: 30,
+                        maxRadius: 50,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Row(
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Maulana Abdul siddiq",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                Text(
+                                  "Joined since 2017",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Text(
+                              "Point : 120",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "Edit Profile",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Icon(Icons.arrow_right),
+                          ],
+                        ),
+                        Divider()
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "Change Password",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Icon(Icons.arrow_right),
+                          ],
+                        ),
+                        Divider()
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "Requested Event",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Icon(Icons.arrow_right),
+                          ],
+                        ),
+                        Divider()
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "History Event",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Icon(Icons.arrow_right),
+                          ],
+                        ),
+                        Divider()
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "Dark Mode",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Switch(
+                              value: isSwitched,
+                              onChanged: (value) {
+                                setState(() {
+                                  isSwitched = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        Divider()
+                      ],
+                    ),
+                  ),
+                  
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
